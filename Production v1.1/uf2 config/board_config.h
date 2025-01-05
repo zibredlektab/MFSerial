0a072f3126ab7c71eaa01ce8de2ffd12bfde0532
zibredlektab <zibredlektab@gmail.com>
2025-01-05 13:57:01 -0500

    Added v1.1 uf2 config files

---
+++ Production v1.1/uf2 config/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS 0
+
+#define VENDOR_NAME "First.AC"
+#define PRODUCT_NAME "MFSerial v1.1"
+#define VOLUME_LABEL "MFSERIAL"
+#define INDEX_URL "http://first.ac"
+#define BOARD_ID "SAMD21G18A-MFSerial-v1.1"
+
+#define USB_VID 0x1209
+#define USB_PID 0x2525
+
+#define LED_PIN PIN_PB03
+
+#endif
