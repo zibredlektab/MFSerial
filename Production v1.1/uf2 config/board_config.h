9074b9623b040d80f3c3bcd95c15993ddfdb9992
zibredlektab <zibredlektab@gmail.com>
2025-05-16 18:15:19 -0400

    Final tweaks for production v1.2, as manufactured

--- Production v1.1/uf2 config/board_config.h
+++ Production v1.1/uf2 config/board_config.h
@@ -4,14 +4,14 @@
 #define CRYSTALLESS 0
 
 #define VENDOR_NAME "First.AC"
-#define PRODUCT_NAME "MFSerial v1.1"
+#define PRODUCT_NAME "MFSerial v1.2"
 #define VOLUME_LABEL "MFSERIAL"
 #define INDEX_URL "http://first.ac"
-#define BOARD_ID "SAMD21G18A-MFSerial-v1.1"
+#define BOARD_ID "SAMD21G18A-MFSerial-v1.2"
 
 #define USB_VID 0x1209
 #define USB_PID 0x2525
 
-#define LED_PIN PIN_PB03
+#define LED_PIN PIN_PA05
 
 #endif
